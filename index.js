2551a13d5495ad559b55818a7f8f21fd9169da4c
sammy-nguyen <Sambu1227@gmail.com>
2021-10-26 14:18:54 -0600

    add step 1 (2)

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-//const Rollbar = require("rollbar");
+const Rollbar = require("rollbar");
 
-// let rollbar = new Rollbar({
-//   accessToken: "",
-//   captureUncaught: true,
-//   captureUnhandledRejections: true,
-// });
+let rollbar = new Rollbar({
+  accessToken: "8a731d2ae65f410589807c3561af65ab",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
-
+  rollbar.info("Congrats, html file showed up ")
 });
 
 
 
-
+app.use(rollbar.errorHandler());
 
 const port = process.env.PORT || 4444;
 
