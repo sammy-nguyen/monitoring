b16d60c2ff9e84f11e2e64738ae3f28c59c3be35
sammy-nguyen <Sambu1227@gmail.com>
2021-10-26 14:09:41 -0600

    add step 1

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+//const Rollbar = require("rollbar");
 
+// let rollbar = new Rollbar({
+//   accessToken: "",
+//   captureUncaught: true,
+//   captureUnhandledRejections: true,
+// });
 
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
-  rollbar.info("html file served successfully");
+
 });
 
 
@@ -16,4 +22,4 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 4444;
 
-app.listen(port, () => console.log(`take us to warp ${port}!`));
+app.listen(port, () => console.log(`running ${port}!`));
